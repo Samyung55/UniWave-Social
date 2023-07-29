4217d6bf294fab72551a958a3f00cbee44a4f9c0
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-29 12:59:29 +0100

    Create LoginScreen.js

---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React, {useContext, useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Platform,
+  StyleSheet,
+  ScrollView
+} from 'react-native';
+
+import FormInput from '../'
