c937723935d9e3ff1dbdffad7688c4f4453a0122
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-29 12:59:20 +0100

    Create PostCard.js

---
+++ components/PostCard.js
@@ -0,0 +1,20 @@
+import React, {useContext, useEffect, useState} from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import {
+  Container,
+  Card,
+  UserInfo,
+  UserImg,
+  UserName,
+  UserInfoText,
+  PostTime,
+  PostText,
+  PostImg,
+  InteractionWrapper,
+  Interaction,
+  InteractionText,
+  Divider,
+} from '../styles/FeedStyles';
+
+import ProgressiveImage from './ProgressiveImage'
