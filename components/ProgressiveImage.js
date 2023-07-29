bc3a9c291c6e85054e849bd3115071f77e93137d
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-29 13:04:58 +0100

    Update ProgressiveImage.js

--- components/ProgressiveImage.js
+++ components/ProgressiveImage.js
@@ -1,3 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {View, StyleSheet, Animated} from 'react-native';
 
+const ProgressiveImage = ({ defaultImageSource, source, style, ...props }) => {
+    const [defaultImageAnimated, setDefaultImageAnimated] = useState(new Animated.Value(0));
+    const [imageAnimated, setImageAnimated] = useState(new Animated.Value(0));
+
+    const handleDefaultImageLoad = () => {
+        Animated.timing(defaultImageAnimated, {
+            toValue: 1,
+            useNativeDriver: true,
+        }).start();
+    };
+
+    const handleImageLoad = () => {
+        Animated.timing(imageAnimated, {
+            toValue: 1,
+            useNativeDriver: true,
+        }).start();
+    }
+}
