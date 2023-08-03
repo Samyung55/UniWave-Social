a92c7ac02c576f722b702fdbd60f7d2f2c155a4b
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-08-03 23:51:48 +0100

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Providers from '../navigation';
+import OnboardingScreen from '../screens/OnboardingScreen'
+
+const App = () => {
+  return (
+    <>
+    <Providers />
+    <OnboardingScreen />
+    </>
+  
+  )
+}
+
+export default App;
